0ffefe7887881044498cad7042b6503d7ff46ede
GregorStocks <gregor@gregorstocks.com>
2017-04-27 14:50:45 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gregor-stocks/libgregor "0.3.0"
+(defproject gregor-stocks/libgregor "0.3.0-SNAPSHOT"
   :description "Stuff Gregor uses"
   :url "https://github.com/GregorStocks/libgregor"
   :license {:name "MIT License"
